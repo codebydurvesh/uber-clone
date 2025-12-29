902731bbd777e88bb52a16f3903eb1fcc8dfaa0b
codebydurvesh <durvesh.gaikwad08@gmail.com>
2025-12-29 14:31:15 +0530

    Initializing

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+export { app };
